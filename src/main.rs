b7740c7193463c060d7db65631ee8cf83a122099
Pravin Ramana <pravinramana25@protonmail.ch>
2022-07-02 16:01:47 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#[path = "common.rs"]
+mod common;
+
+#[path = "twitch.rs"]
+mod twitch;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/twitch.rs
@@ -0,0 +1,4 @@
+pub struct VOD {
+    id: u32,
+    title: String,
+}
